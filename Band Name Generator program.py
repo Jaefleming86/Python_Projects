9d2c37f3ad1af4c8cb0bfc938659ca19c57eee5a
Mel Raymond <130794164+dn30001@users.noreply.github.com>
2023-10-04 09:42:43 -0700

    Commit 10.2.23

---
+++ Band Name Generator program.py
@@ -0,0 +1,10 @@
+# - Band Name Generator program.  This program will generate a cool band name based on your hometown and you favorite animal.
+
+print("This is your Band Name Generator.")
+print("")
+print("Please answer the following questions...")
+
+city = input("What is the name of your hometown?: " + "\n")
+animal = input("What is your favorite animal?: " + "\n")
+
+print("Your band name is " + city + " " + animal + ".")
